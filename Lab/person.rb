059592e2edd61f8ab2955c97d788a14d50dfd217
Cori Allen <coriallen@gmail.com>
2013-09-28 18:23:14 -0400

    Creates Person class.

--- Lab/person.rb
+++ Lab/person.rb
@@ -1,3 +1,18 @@
-class Person
+# - Client:
+		# - A client should have a name.
+		# - A client should have an age.
+		# - A client should have a number of pets.
+#   - A client should have a hash of pets (with pet names as keys and animal (object) as values)
+#   - A client should be able to accept a pet and add it to their pets list
+#   - A client should be able to give up a pet from their list
 
+class Person
+	attr_accessor :name, :age, :number_of_pets, :pet_hash
+
+	def initialize(name, age, number_of_pets)
+		@name = name
+		@age = age
+		@number_of_pets = :number_of_pets #hmm...need to += and -= this
+		@pet_hash = {} #:pet_name => animal (object) ###instantiated in animal.rb
+	end
 end
