cc24bcf00c36f9c76804e3da7c949cb988c7b405
Cori Allen <coriallen@gmail.com>
2013-09-28 18:43:36 -0400

    Creates Shelter class, updates Person class, start on class methods.

--- Lab/person.rb
+++ Lab/person.rb
@@ -1,10 +1,3 @@
-# - Client:
-		# - A client should have a name.
-		# - A client should have an age.
-		# - A client should have a number of pets.
-#   - A client should have a hash of pets (with pet names as keys and animal (object) as values)
-#   - A client should be able to accept a pet and add it to their pets list
-#   - A client should be able to give up a pet from their list
 
 class Person
 	attr_accessor :name, :age, :number_of_pets, :pet_hash
@@ -12,7 +5,17 @@ class Person
 	def initialize(name, age, number_of_pets)
 		@name = name
 		@age = age
-		@number_of_pets = :number_of_pets #hmm...need to += and -= this
+		@number_of_pets = :number_of_pets #hmm...need to += and -= this...array?
 		@pet_hash = {} #:pet_name => animal (object) ###instantiated in animal.rb
 	end
+
+#   - A client should be able to accept a pet and add it to their pets list
+	# def get_pet
+
+	# end
+
+#   - A client should be able to give up a pet from their list 
+	# def give_pet_to_shelter
+
+	# end 
 end
--- Lab/shelter.rb
+++ Lab/shelter.rb
@@ -1,3 +1,30 @@
+
 class Shelter
+	attr_accessor :client_list, :animal_list #dont give writer to shelter's name, address
+											# do I want to give outside access to
+											#read and write client & animal list? hmm...
+	def initialize(name, address)
+		@name = name
+		@address = address
+		@client_list = [] #should this be a hash, person attatched to animal?
+		@animal_list = [] #might be better as a hash b/c not an ordered list?
+	end
+
+	#   - The shelter should be able to accept an animal (add to it's list).
+	def add_animal(animal)
+
+	end
+
+	#   - The shelter should be able to accept a client (add to it's list).
+	def add_client
+
+	end
+
+	##adopt out an animal to person
+	def give_animal
+
+	end
+
+
 
 end
